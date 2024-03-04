ea45875f723cc3850210ae142a8ecd30fddf2c92
Hieu Ngan <156532354+nagn453@users.noreply.github.com>
2024-03-04 07:53:31 +0700

    Create buoi5-2.py

---
+++ buoi5-2.py
@@ -0,0 +1,12 @@
+def count_word_frequency(words):
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else: 
+            word_count[word] = 1
+    return word_count
+    
+words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple']
+count = count_word_frequency(words)
+print(count)
