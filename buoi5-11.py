a45f23ada07573bf11fadc1a106c31a1b575495f
Hieu Ngan <156532354+nagn453@users.noreply.github.com>
2024-03-04 08:21:07 +0700

    Create buoi5-11.py

---
+++ buoi5-11.py
@@ -0,0 +1,5 @@
+def concatenate_string(input_tuple):
+    return ' '.join(input_tuple)
+input_tuple = ('Hello', 'Wolrd', 'from', 'Python')
+output_string = concatenate_string(input_tuple)
+print(output_string)
